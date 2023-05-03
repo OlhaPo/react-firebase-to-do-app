fd9365f21c46ebec6de5b7797eaa022b6ce90fd8
Olya <ponomaryova.olya.y@gmail.com>
2023-05-03 13:33:05 +0200

    Add styling to components.

--- src/ToDo.jsx
+++ src/ToDo.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import { MdDelete } from "react-icons/md";
 
 const style = {
-  li: ``,
-  row: ``,
-  text: ``,
+  li: `flex justify-between bg-slate-200 p-4 my-2 capitalize`,
+  liComplete: `flex justify-between bg-slate-400 p-4 my-2 capitalize `,
+  row: `flex`,
+  text: `ml-2 cursor-pointer `,
+  textComplete: `ml-2 cursor-pointer  line-through`,
+  button: `cursor-pointer flex items-center`,
 };
 
 const ToDo = ({ todo }) => {
